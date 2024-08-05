1176a4ee276f4e28c30eb3ff727b44f77a807e0b
Celeste Liu <CoelacanthusHex@gmail.com>
2024-08-05 22:17:14 +0800

    mailbox: th1520: missing header <linux/platform_device.h>
    Fixes: 4733498537ae ("mailbox: add XuanTie TH1520 Mailbox IPC driver")
    Signed-off-by: Celeste Liu <CoelacanthusHex@gmail.com>

--- drivers/mailbox/th1520-mailbox.c
+++ drivers/mailbox/th1520-mailbox.c
@@ -15,6 +15,7 @@
 #include <linux/mailbox_controller.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <linux/platform_device.h>
 #include <linux/slab.h>
 
 /* Status Register */
