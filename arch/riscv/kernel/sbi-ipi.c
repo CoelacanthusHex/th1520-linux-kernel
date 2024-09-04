6c019b5c3331f829fb8b1fb483448b641c80fb08
Guo Ren <guoren@linux.alibaba.com>
2024-09-04 15:11:50 +0800

    riscv: Add ACLINT SSWI support
    The SSWI device provides supervisor-level IPI functionality for a
    set of HARTs on a RISC-V platform. It provides a register to set
    an IPI (SETSSIP) for each HART connected to the SSWI device.
    
    The patch utilizes the feature to optimize IPI handling by avoiding
    Linux calls into firmware runtime, thus minimizing context switching
    expenses and removing the dependency on sbi-ipi.
    
    Co-developed-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>
    Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- arch/riscv/kernel/sbi-ipi.c
+++ arch/riscv/kernel/sbi-ipi.c
@@ -3,20 +3,37 @@
  * Multiplex several IPIs over a single HW IPI.
  *
  * Copyright (c) 2022 Ventana Micro Systems Inc.
+ * Copyright (C) 2024 Alibaba Group Holding Limited.
  */
 
 #define pr_fmt(fmt) "riscv: " fmt
 #include <linux/cpu.h>
 #include <linux/init.h>
 #include <linux/irq.h>
+#include <linux/irqchip.h>
 #include <linux/irqchip/chained_irq.h>
 #include <linux/irqdomain.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
 #include <asm/sbi.h>
 
 DEFINE_STATIC_KEY_FALSE(riscv_sbi_for_rfence);
 EXPORT_SYMBOL_GPL(riscv_sbi_for_rfence);
 
 static int sbi_ipi_virq;
+
+static u32 __iomem *sswi_base;
+
+static void sswi_send_ipi(unsigned int cpu)
+{
+	writel(1, sswi_base + cpuid_to_hartid_map(cpu));
+}
+
+static void sswi_clear_ipi(void)
+{
+	writel(0, sswi_base + cpuid_to_hartid_map(smp_processor_id()));
+}
 
 static void sbi_ipi_handle(struct irq_desc *desc)
 {
@@ -25,6 +42,9 @@ static void sbi_ipi_handle(struct irq_desc *desc)
 	chained_irq_enter(chip, desc);
 
 	csr_clear(CSR_IP, IE_SIE);
+	if (sswi_base)
+		sswi_clear_ipi();
+
 	ipi_mux_process();
 
 	chained_irq_exit(chip, desc);
@@ -57,7 +77,8 @@ void __init sbi_ipi_init(void)
 		return;
 	}
 
-	virq = ipi_mux_create(BITS_PER_BYTE, sbi_send_ipi);
+	virq = ipi_mux_create(BITS_PER_BYTE, sswi_base ? sswi_send_ipi
+							: sbi_send_ipi);
 	if (virq <= 0) {
 		pr_err("unable to create muxed IPIs\n");
 		irq_dispose_mapping(sbi_ipi_virq);
@@ -72,6 +93,8 @@ void __init sbi_ipi_init(void)
 	 * via generic IPI-Mux
 	 */
 	cpuhp_setup_state(CPUHP_AP_IRQ_RISCV_SBI_IPI_STARTING,
+			  sswi_base ?
+			  "irqchip/sswi-ipi:starting" :
 			  "irqchip/sbi-ipi:starting",
 			  sbi_ipi_starting_cpu, NULL);
 
@@ -84,3 +107,16 @@ void __init sbi_ipi_init(void)
 	 */
 	static_branch_enable(&riscv_sbi_for_rfence);
 }
+
+static int __init aclint_sswi_probe(struct device_node *node,
+				    struct device_node *parent)
+{
+	sswi_base = of_iomap(node, 0);
+	if (!sswi_base) {
+		pr_err("RISC-V ACLINT SSWI device probe failure\n");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+IRQCHIP_DECLARE(riscv_aclint_sswi, "riscv,aclint-sswi", aclint_sswi_probe);
