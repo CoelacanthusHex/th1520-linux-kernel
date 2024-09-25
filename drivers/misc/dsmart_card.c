949fe48d16e051ec4be3062bdd4c95aa240a120b
Celeste Liu <CoelacanthusHex@gmail.com>
2024-09-26 03:39:27 +0800

    misc/dsmart_card: missing header
    Signed-off-by: Celeste Liu <CoelacanthusHex@gmail.com>

--- drivers/misc/dsmart_card.c
+++ drivers/misc/dsmart_card.c
@@ -11,6 +11,7 @@
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
